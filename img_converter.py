82fc6ce2be025825f0e010712a1e73bf79813d8b
Jackyjz <jackyjz.zhao@mail.utoronto.ca>
2025-03-24 00:15:20 -0400

    bg done, character can move up and down

--- img_converter.py
+++ img_converter.py
@@ -2,58 +2,46 @@ import numpy as np
 import cv2
 import pyperclip
 
-
 def convert_img(file_name, img_name, width, height):
-    """
-    Convert an image to a C-style array.
+    img = cv2.imread(file_name, cv2.IMREAD_UNCHANGED)  # Preserve alpha if present
 
-    The image is resized, converted to a 16-bit color format (5-6-5 RGB), flattened,
-    and copied to the clipboard as a C array.
+    if img.shape[2] == 4:
+        # Has alpha channel
+        b, g, r, a = cv2.split(img)
+        alpha_mask = a > 0
+        img = cv2.merge((b, g, r))  # Drop alpha for RGB processing
+    else:
+        # No alpha: consider pure black (0,0,0) as transparent
+        alpha_mask = ~(np.all(img == [0, 0, 0], axis=-1))
 
-    :param file_name: Path to the input image.
-    :param img_name: Name of the output C array.
-    :param width: Target width of the image.
-    :param height: Target height of the image.
-    """
-    img = cv2.imread(file_name).astype(np.uint16)
     img = cv2.resize(img, (width, height), interpolation=cv2.INTER_NEAREST)
-    
-    # Convert BGR to 5-bit Red, 6-bit Green, and 5-bit Blue
+    alpha_mask = cv2.resize(alpha_mask.astype(np.uint8), (width, height), interpolation=cv2.INTER_NEAREST).astype(bool)
+
+    img = img.astype(np.uint16)
     img_r = img[:, :, 2] >> 3
     img_g = img[:, :, 1] >> 2
     img_b = img[:, :, 0] >> 3
-    
-    # print("First pixel values:", img_r[0, 0], img_g[0, 0], img_b[0, 0])
-    
-    img = (img_r << 11) + (img_g << 5) + img_b
-    # print("Converted first pixel value:", img[0, 0])
-    
-    img = img.flatten()
-    img_array = ', '.join(f"0x{x:04X}" for x in img)
+    img_rgb565 = (img_r << 11) + (img_g << 5) + img_b
+
+    # Set transparent background pixels to a magic value (e.g., 0xDEAD)
+    img_rgb565[~alpha_mask] = 0xDEAD
+
+    img_flat = img_rgb565.flatten()
+    img_array = ', '.join(f"0x{x:04X}" for x in img_flat)
     output_code = f"short unsigned int {img_name}[{width * height}] = {{{img_array}}};"
-    
-    # Generate and append drawing function code
     output_code += "\n\n" + generate_draw_pixel_code(img_name, width, height)
-    
-    # Copy to clipboard
     pyperclip.copy(output_code)
     print("C array copied to clipboard.")
 
 
 def generate_draw_pixel_code(img_name, width, height):
-    """
-    Generate C functions to plot and erase an image using plot_pixel().
-
-    :param img_name: Name of the image array.
-    :param width: Image width.
-    :param height: Image height.
-    :return: C function code as a string.
-    """
     return f"""
 void plot_image_{img_name}(int x, int y) {{
     for (int i = 0; i < {height}; i++) {{
         for (int j = 0; j < {width}; j++) {{
-            plot_pixel(x + j, y + i, {img_name}[i * {width} + j]);
+            unsigned short color = {img_name}[i * {width} + j];
+            if (color != 0xDEAD)  // Skip transparent
+                plot_pixel(x + j, y + i, color);
         }}
     }}
 }}
@@ -61,7 +49,8 @@ void plot_image_{img_name}(int x, int y) {{
 void erase_image_{img_name}(int x, int y) {{
     for (int i = 0; i < {height}; i++) {{
         for (int j = 0; j < {width}; j++) {{
-            plot_pixel(x + j, y + i, 0);
+            if ({img_name}[i * {width} + j] != 0xDEAD)
+                plot_pixel(x + j, y + i, 0);
         }}
     }}
 }}
@@ -69,8 +58,8 @@ void erase_image_{img_name}(int x, int y) {{
 
 if __name__ == '__main__':
     convert_img(
-        file_name=r'mario.png',
-        img_name='mario',
-        width=28,
-        height=32
+        file_name=r'bg.png',
+        img_name='bg',
+        width=320,
+        height=240
     )
